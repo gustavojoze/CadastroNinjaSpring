b3dc9e60ec809b14f420c5030af2380564897f24
gustavojoze <gustavojosesoaressantana@gmail.com>
2025-07-26 18:05:09 -0300

    first commit

---
+++ src/main/java/com/empresa/CadastroNinjaSpring/CadastroNinjaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.empresa.CadastroNinjaSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroNinjaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroNinjaSpringApplication.class, args);
+	}
+
+}
