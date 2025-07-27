ad4c32eb8947ce62f886d5fa6ddd9230bde6684f
gustavojoze <gustavojosesoaressantana@gmail.com>
2025-07-26 22:37:10 -0300

    Cria uma entidade no banco de dados com o NinjaModel

---
+++ src/main/java/com/empresa/CadastroNinjaSpring/Ninja/NinjaModel.java
@@ -0,0 +1,55 @@
+package com.empresa.CadastroNinjaSpring.Ninja;
+
+import com.empresa.CadastroNinjaSpring.Missoes.MissoesModel;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_cadastro")
+public class NinjaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+    //Um ninja só pode estar ligado apenas em uma missão
+    @ManyToOne
+    @JoinColumn(name="missoes_id") // Foreing Key ou chave estrangeira
+    private MissoesModel missao;
+
+
+    public NinjaModel(){
+
+    }
+
+    public NinjaModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
